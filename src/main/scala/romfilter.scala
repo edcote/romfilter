7c592c51de4f89363701271c2db1c5cab37dd2c2
Edmond Cote <edmond.cote@gmail.com>
2018-10-22 17:32:49 -0700

    wip

--- src/main/scala/romfilter.scala
+++ src/main/scala/romfilter.scala
@@ -1,24 +1,39 @@
 package romfilter
 
-case class ROMInfo(name: String, cloneof: String, descrption: String, publisher: String, year: String)
+import scala.io.Source
+
+case class ROMInfo(name: String, cloneof: String, description: String, publisher: String, year: String)(implicit val invalidPublishers: Set[String]) {
+  def isClone: Boolean = cloneof != ""
+
+  def valid: Boolean = {
+    !isClone
+  }
+}
 
 class ROMFilter {
-  private val illegalPublishers =
+  implicit private val invalidPublishers =
     """
       |&lt;unknown&gt;
       |<unknown>
       |<homebrew>
       |<doujin>
     """.stripMargin.trim.split("\n").toSet
+
+  private val catverFile = getClass.getResource("/catver.ini").getFile
+  // FIXME: bug in expression
+  private val categories = Source.fromFile(catverFile)
+    .getLines
+    .filter(_.trim.nonEmpty)
+    .filterNot(_.startsWith(Seq(";;", "[")))
+    .map(_.split("=", 2))
+    .map { s =>
+      (s(0), s(1).trim)
+    }
+    .toMap
+
   private val xmlFile = getClass.getResource("/listsoftware.0188.xml").getFile
   private val softwarelists = scala.xml.XML.loadFile(xmlFile)
   private val software = softwarelists \ "softwarelist" \ "software"
-
-  def illegalFilter(info: ROMInfo): Boolean = {
-    illegalPublishers.contains(info.publisher) ||
-      info.cloneof != ""
-
-  }
 
   private val db = software
     .map { node =>
@@ -30,8 +45,8 @@ class ROMFilter {
       ROMInfo(name, cloneof, description.text, publisher.text, year.text)
     }
     .filter {
-      case info if illegalFilter(info) => false
-      case _ => true
+      case rom if rom.valid => true
+      case _ => false
     }
 
   // FIXME: write to file
