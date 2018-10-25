55c471007e72e8e0554666b61b1db0c5bfab3465
Edmond Cote <edmond.cote@gmail.com>
2018-10-24 19:36:32 -0700

    wip

--- src/main/scala/romfilter.scala
+++ src/main/scala/romfilter.scala
@@ -2,7 +2,7 @@ package romfilter
 
 import scala.io.Source
 
-case class ROMInfo(name: String, cloneof: String, description: String, publisher: String, year: String)(implicit val invalidPublishers: Set[String]) {
+case class ROMInfo(name: String, cloneof: String, description: String, publisher: String, year: String, category: String) {
   def isClone: Boolean = cloneof != ""
 
   def valid: Boolean = {
@@ -10,27 +10,27 @@ case class ROMInfo(name: String, cloneof: String, description: String, publisher
   }
 }
 
-class ROMFilter {
-  implicit private val invalidPublishers =
-    """
-      |&lt;unknown&gt;
-      |<unknown>
-      |<homebrew>
-      |<doujin>
-    """.stripMargin.trim.split("\n").toSet
+class ROMFilter(invalidPublishers: Seq[String], invalidCategories: Seq[String]) {
+  // Load and parse category file
+  private def validCategory(category: String): Boolean =
+    invalidPublishers.count { invalid =>
+      !category.contains(invalid)
+    } > 0
 
   private val catverFile = getClass.getResource("/catver.ini").getFile
-  // FIXME: bug in expression
-  private val categories = Source.fromFile(catverFile)
+
+  private val category: Map[String, String] = Source.fromFile(catverFile)
     .getLines
     .filter(_.trim.nonEmpty)
-    .filterNot(_.startsWith(Seq(";;", "[")))
-    .map(_.split("=", 2))
-    .map { s =>
-      (s(0), s(1).trim)
+    .filterNot(_.startsWith(";;"))
+    .filterNot(_.startsWith("[Category]"))
+    .map { x =>
+      val y = x.split("=")
+      y(0) -> y(1)
     }
     .toMap
 
+  // Load and parse MAME XML database
   private val xmlFile = getClass.getResource("/listsoftware.0188.xml").getFile
   private val softwarelists = scala.xml.XML.loadFile(xmlFile)
   private val software = softwarelists \ "softwarelist" \ "software"
@@ -42,19 +42,46 @@ class ROMFilter {
       val description = node \ "description"
       val publisher = node \ "publisher"
       val year = node \ "year"
-      ROMInfo(name, cloneof, description.text, publisher.text, year.text)
+      ROMInfo(name, cloneof, description.text, publisher.text, year.text, category.getOrElse(name, "Unknown"))
     }
     .filter {
-      case rom if rom.valid => true
+      case rom if rom.valid && validCategory(category.getOrElse(rom.name, "")) => true
       case _ => false
     }
 
-  // FIXME: write to file
-  db.foreach(println)
 }
 
 object ROMFilter {
   def main(args: Array[String]): Unit = {
-    val romFilter = new ROMFilter()
+    val publishers =
+      """
+        |&lt;unknown&gt;
+        |<unknown>
+        |<homebrew>
+        |<doujin>
+      """.stripMargin.trim.split(System.lineSeparator())
+
+    val categories =
+      """
+        |Ball & Paddle
+        |Casino
+        |Puzzle / Sliding
+        |Misc.
+        |Misc. Betting
+        |Sports / Darts
+        |Sports / Horse Racing
+        |Sports / Horseshoes
+        |Tabletop
+        |Unplayable
+        |Mahjong
+        |Rhythm
+        |Puzzle / Cards
+        |Maze
+        |Quiz
+        |BIOS
+        |Mature
+      """.stripMargin.trim.split(System.lineSeparator())
+
+    val romFilter = new ROMFilter(publishers, categories)
   }
 }
